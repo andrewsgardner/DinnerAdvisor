d8ebb9cb2c34b6f0f6a9109dbbdc6e76007d9462
Andrew S. Gardner <asg@andrewsgardner.com>
2017-06-06 23:26:44 -0400

    Add required node modules

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // load required node modules
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('mongojs');
+var db = mongojs('dinneradvisor',['meals']);
