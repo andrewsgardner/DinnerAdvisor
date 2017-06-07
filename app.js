9c5fdb955e7393cc7494b11f4b0aafab2e6c9285
Andrew S. Gardner <asg@andrewsgardner.com>
2017-06-06 23:38:04 -0400

    Add middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// load required node modules
+// LOAD REQUIRED NODE MODULES
+// ==========================
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('mongojs');
 var db = mongojs('dinneradvisor',['meals']);
+
+// MIDDLEWARE
+// ==========
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client')));
