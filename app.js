548a2de8b65fc90c3139fdb6c5a1396735159eed
Andrew S. Gardner <asg@andrewsgardner.com>
2017-06-25 15:20:35 -0400

    Add API call to get all recipes from database

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('mongojs');
-var db = mongojs('dinneradvisor',['meals']);
+var db = mongojs('dinneradvisor',['recipes']);
 
 // MIDDLEWARE
 // ==========
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client')));
+
+// API ROUTES
+// ==========
+// return all recipes from the database
+app.get('/recipes', function(req, res){
+  db.recipes.find(function(err, docs){
+    if(err){
+      res.send(err);
+    } else {
+      console.log('Getting All Recipes...');
+      res.json(doc);
+    }
+  });
+});
