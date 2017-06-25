d0a0a4a3c9f060caf171be6de283ef1b58f212f1
Andrew S. Gardner <asg@andrewsgardner.com>
2017-06-25 15:32:52 -0400

    Enter API call to add new recipes into the database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // LOAD REQUIRED NODE MODULES
 // ==========================
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -8,11 +9,13 @@ var db = mongojs('dinneradvisor',['recipes']);
 
 // MIDDLEWARE
 // ==========
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client')));
 
 // API ROUTES
 // ==========
+
 // return all recipes from the database
 app.get('/recipes', function(req, res){
   db.recipes.find(function(err, docs){
@@ -20,6 +23,18 @@ app.get('/recipes', function(req, res){
       res.send(err);
     } else {
       console.log('Getting All Recipes...');
+      res.json(doc);
+    }
+  });
+});
+
+// enter new recipes into the database
+app.post('/recipes', function(req, res){
+  db.recipes.insert(req.body, function(err, doc){
+    if(err){
+      res.send(err);
+    } else {
+      console.log('Adding Recipe...');
       res.json(doc);
     }
   });
