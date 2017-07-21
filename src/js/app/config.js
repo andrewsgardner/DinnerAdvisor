52447a3528db7a04e0d7a87a5fbbd47c743022fe
Andrew S. Gardner <asg@andrewsgardner.com>
2017-07-20 22:50:17 -0400

    Add config for KnockoutJS

--- src/js/app/config.js
+++ src/js/app/config.js
@@ -2,7 +2,13 @@ requirejs.config({
   'baseUrl': './js/app',
   'paths': {
     // load core libraries
-    'jquery': '../lib/jquery-3.2.1'
+    'jquery': '../lib/jquery-3.2.1',
+    'ko': '../lib/knockout-3.4.2'
+  },
+  shim: {
+    ko: {
+      exports: 'ko'
+    }
   }
 });
 
