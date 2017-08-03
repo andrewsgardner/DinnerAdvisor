e7125f5e116298c51730a90fd0698e251a60b790
Andrew S. Gardner <asg@andrewsgardner.com>
2017-08-02 22:57:32 -0400

    Add jquery shim config

--- src/js/app/config.js
+++ src/js/app/config.js
@@ -3,12 +3,15 @@ requirejs.config({
   'paths': {
     // load core libraries
     'jquery': '../lib/jquery-3.2.1',
-    'ko': '../lib/knockout-3.4.2',
+    'knockout': '../lib/knockout-3.4.2',
     'bootstrap': '../lib/bootstrap-3.3.7'
   },
   shim: {
     ko: {
       exports: 'ko'
+    },
+    jquery: {
+      exports: '$'
     },
     bootstrap: {
       deps: 'jquery'
