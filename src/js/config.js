85de4a4bcc9a792bba3f6ce1fd7fc3f0d0e30d19
Andrew S. Gardner <asg@andrewsgardner.com>
2017-09-06 22:29:42 -0400

    Update baseUrl and load main app module

---
+++ src/js/config.js
@@ -0,0 +1,23 @@
+requirejs.config({
+  "baseUrl": "js/lib",
+  "paths": {
+    // load core libraries
+    "jquery": "jquery-3.2.1",
+    "knockout": "knockout-3.4.2",
+    "bootstrap": "bootstrap-3.3.7",
+    "app": "../app"
+  },
+  shim: {
+    "ko": {
+      "exports": [ "ko" ]
+    },
+    "jquery": {
+      "exports": [ "$" ]
+    },
+    "bootstrap": {
+      "deps": [ "jquery" ]
+    }
+  }
+});
+
+requirejs(["app/app"]);
